f0cbc8267fb33ee17017a0552a1c9bc0ed518e46
mazda <mazda@aduki.co.uk>
2024-12-10 15:57:44 +0100

    Day 10 part 2

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -1,11 +1,13 @@
 use grid::*;
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 fn main() {
     let input_file = "input.txt";
     let grid_str = std::fs::read_to_string(input_file).unwrap();
     let (grid, summits, trail_heads) = load_grid_from_str(&grid_str);
-    let total_summits_reached = find_summits(&grid, summits, trail_heads);
+    let total_summits_reached = find_summits(&grid, &summits, &trail_heads);
     println!("Part1: {:?}", total_summits_reached);
+    let total_trails = find_trails(&grid, &summits, &trail_heads);
+    println!("Part2: {:?}", total_trails);
 }
 
 type Summits = HashSet<(usize, usize)>;
@@ -35,11 +37,11 @@ fn load_grid_from_str(input: &str) -> (Grid<u8>, Summits, TrailHeads) {
     (grid, summits, trail_heads)
 }
 
-fn find_summits(grid: &Grid<u8>, summits: Summits, trail_heads: TrailHeads) -> usize {
+fn find_summits(grid: &Grid<u8>, summits: &Summits, trail_heads: &TrailHeads) -> usize {
     //bfs to find the number of summits reachable from the trail heads
 
     let mut num_summits_reached = 0;
-    for (i, j) in trail_heads {
+    for (i, j) in trail_heads.clone() {
         let mut visited = HashSet::new();
         let mut queue = VecDeque::new();
         queue.push_back((i, j, 0));
@@ -66,6 +68,54 @@ fn find_summits(grid: &Grid<u8>, summits: Summits, trail_heads: TrailHeads) -> u
 
     num_summits_reached
 }
+fn find_trails(grid: &Grid<u8>, summits: &Summits, trail_heads: &TrailHeads) -> usize {
+    let mut trails = 0;
+
+    for &(i, j) in trail_heads.iter() {
+        let mut times_visited: HashMap<(usize, usize), usize> = HashMap::new();
+        times_visited.insert((i, j), 1);
+
+        let mut queue = VecDeque::new();
+        queue.push_back((i, j, grid.get(i, j).unwrap().clone()));
+
+        while let Some((i, j, height)) = queue.pop_front() {
+            if summits.contains(&(i, j)) {
+                if let Some(&count) = times_visited.get(&(i, j)) {
+                    trails += count;
+                }
+            }
+
+            for &(di, dj) in &[(0, 1), (1, 0), (0, -1), (-1, 0)] {
+                let new_i = i as isize + di;
+                let new_j = j as isize + dj;
+
+                if new_i >= 0 && new_j >= 0 && new_i < grid.rows() as isize && new_j < grid.cols() as isize {
+                    let new_i = new_i as usize;
+                    let new_j = new_j as usize;
+
+                    if let Some(&new_height) = grid.get(new_i, new_j) {
+                        if new_height == height + 1 {
+                            if let Some(seen_count) = times_visited.get(&(new_i, new_j)) {
+                                if let Some(&current_count) = times_visited.get(&(i, j)) {
+                                    times_visited.insert((new_i, new_j),seen_count + current_count);
+                                }
+                                continue;
+                            }
+
+                            if let Some(&current_count) = times_visited.get(&(i, j)) {
+                                times_visited.insert((new_i, new_j), current_count);
+                            }
+
+                            queue.push_back((new_i, new_j, new_height));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    trails
+}
 
 #[cfg(test)]
 mod tests {
@@ -88,7 +138,7 @@ mod tests {
     fn can_find_path() {
         let grid_str = "0123\n1234\n8765\n9876";
         let (grid, summits, trail_heads) = load_grid_from_str(grid_str);
-        let total_summits_reached = find_summits(&grid, summits, trail_heads);
+        let total_summits_reached = find_summits(&grid, &summits, &trail_heads);
         assert_eq!(total_summits_reached, 1)
     }
 
@@ -97,7 +147,7 @@ mod tests {
         let input_file = "test_input.txt";
         let grid_str = std::fs::read_to_string(input_file).unwrap();
         let (grid, summits, trail_heads) = load_grid_from_str(&grid_str);
-        let total_summits_reached = find_summits(&grid, summits, trail_heads);
+        let total_summits_reached = find_summits(&grid, &summits, &trail_heads);
         assert_eq!(total_summits_reached, 36)
     }
     #[test]
@@ -107,7 +157,16 @@ mod tests {
         let (grid, summits, trail_heads) = load_grid_from_str(&grid_str);
         assert_eq!(summits.len(), 2);
         assert_eq!(trail_heads.len(), 1);
-        let total_summits_reached = find_summits(&grid, summits, trail_heads);
+        let total_summits_reached = find_summits(&grid, &summits, &trail_heads);
         assert_eq!(total_summits_reached, 2);
+    }
+
+    #[test]
+    fn test_count_trails(){
+        let input_file = "test_input.txt";
+        let grid_str = std::fs::read_to_string(input_file).unwrap();
+        let (grid, summits, trail_heads) = load_grid_from_str(&grid_str);
+        let total_trails = find_trails(&grid, &summits, &trail_heads);
+        assert_eq!(total_trails, 81)
     }
 }
