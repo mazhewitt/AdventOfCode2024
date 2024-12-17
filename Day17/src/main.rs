b3e92599b2ae7aa8bbfad6c71103d33930012751
mazhewitt <mazda@aduki.co.uk>
2024-12-17 21:47:35 +0100

    After looking at Reddit, we finally have part 2

--- Day17/src/main.rs
+++ Day17/src/main.rs
@@ -89,7 +89,52 @@ impl Computer {
         self.register_c = self.register_a >> self.get_combo_value(operand);
         self.ip += 2;
     }
+
+
+    fn reverse_eng(&mut self)  {
+        self.output = Vec::new();
+        while self.ip < self.program.len() {
+            let opcode = self.program[self.ip];
+            let operand = self.program[self.ip + 1];
+            match opcode {
+                0 => self.adv(operand),
+                1 => self.bxl(operand),
+                2 => self.bst(operand),
+                3 => panic!("No idea what to do here, but I dont have one of these"),
+                4 => self.bxc(),
+                5 => {
+                    self.out(operand);
+                    break; // break because we have to re-run for each output
+                }
+                6 => self.bdv(operand),
+                7 => self.cdv(operand),
+                _ => break,
+            }
+        }
+    }
+    
+    
 }
+fn solve_for_a(target: &[u8], a: usize, program: &[u8]) -> Option<usize> {
+    if target.is_empty() {
+        return Some(a);
+    }
+
+    for t in 0..8 {
+        let candidate_a = (a << 3) | t;
+        let mut computer = Computer::new(candidate_a, 0, 0, program.to_vec());
+        computer.reverse_eng();
+        if computer.output.len() >0 {
+            if computer.output.last() == target.last() {
+                if let Some(result) = solve_for_a(&target[..target.len() - 1], candidate_a, program) {
+                    return Some(result);
+                }
+            }
+        }
+    }
+    None
+}
+
 
 fn main() {
     let initial_a = 37293246;
@@ -97,7 +142,7 @@ fn main() {
     let initial_c = 0;
     let program = vec![2,4,1,6,7,5,4,4,1,7,0,3,5,5,3,0]; // Replace with puzzle input
 
-    let mut computer = Computer::new(initial_a, initial_b, initial_c, program);
+    let mut computer = Computer::new(initial_a, initial_b, initial_c, program.clone());
     computer.run();
     let output = computer.output.iter()
         .map(|x| x.to_string())
@@ -105,4 +150,12 @@ fn main() {
         .join(",");
 
     println!("{}", output);
+
+    
+
+    if let Some(a) = solve_for_a(&program, 0, &program) {
+        println!("The lowest positive value of A is: {}", a);
+    } else {
+        println!("No solution found");
+    }
 }
